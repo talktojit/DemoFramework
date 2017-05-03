31bf72c37e7e602abb91bfce4db3876979b7e312
sujit <talktojit@gmail.com>
2017-05-03 22:51:34 +0530

    Deleted warning statements

--- projFramework/src/test/java/common/BrowserHelper.java
+++ projFramework/src/test/java/common/BrowserHelper.java
@@ -1,7 +1,6 @@
 package common;
 
 import java.io.File;
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
--- projFramework/src/test/java/common/TestConfig.java
+++ projFramework/src/test/java/common/TestConfig.java
@@ -5,7 +5,6 @@ import java.io.FileInputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Properties;
-import java.util.TimeZone;
 
 public class TestConfig
 {
